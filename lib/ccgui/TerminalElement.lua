04e3fa2e25a5d4949663ea250f14f8ac67ab79e7
Mattias Buelens <mattias@buelens.com>
2014-01-08 12:44:46 +0100

    Moved terminal methods to ElementTerminal.
    Was conflicting with Element:clear.

--- lib/ccgui/TerminalElement.lua
+++ lib/ccgui/TerminalElement.lua
@@ -1,48 +1,41 @@
 --[[
 
 	ComputerCraft GUI
-	Terminal element
+	Element hosting a terminal
 
 --]]
 
 local Element			= require "ccgui.Element"
-local BufferedScreen	= require "ccgui.paint.BufferedScreen"
+local ElementTerminal	= require "ccgui.paint.ElementTerminal"
 
+-- Element hosting a terminal
 local TerminalElement = Element:subclass("ccgui.TerminalElement")
 function TerminalElement:initialize(opts)
 	super.initialize(self, opts)
-	-- Screen
-	self.screen = BufferedScreen:new(0, 0)
-	-- Terminal state
-	self.curX, self.curY = 1, 1
-	self.text = colours.white
-	self.back = colours.black
-	self.blink = false
-	-- Delegate terminal methods
-	for k,f in pairs(term) do
-		if self[k] == nil then
-			self[k] = function(self, ...)
-				return f(...)
-			end
-		end
-	end
-	
+
+	self.term = ElementTerminal:new(self)
+
 	self:on("focus", self.updateTerminalBlink, self)
 	self:on("blur", self.updateTerminalBlink, self)
-	
+
 	self:on("beforepaint", self.terminalResize, self)
 	self:on("paint", self.terminalPaint, self)
 end
-	
+
+function TerminalElement:asTerm()
+	return self.term:export()
+end
+
 function TerminalElement:canFocus()
 	return self:visible()
 end
 
 function TerminalElement:showTerminalBlink()
-	super:setCursorBlink(self.blink, self.curX, self.curY, self.text)
+	local bbox = self:inner(self.bbox)
+	self:setCursorBlink(self.term:getBlinkState(bbox))
 end
 function TerminalElement:hideTerminalBlink()
-	super:setCursorBlink(false)
+	self:setCursorBlink(false)
 end
 function TerminalElement:updateTerminalBlink()
 	if self.hasFocus then
@@ -52,99 +45,17 @@ function TerminalElement:updateTerminalBlink()
 	end
 end
 
-function TerminalElement:asTerm()
-	-- Create delegate which matches the term API
-	local exported = {}
-	for k,v in pairs(term) do
-		-- Redirect to own method
-		exported[k] = function(...)
-			return self[k](self, ...)
-		end
-	end
-	return exported
-end
-
-function TerminalElement:getWidth()
-	return self.screen.width
-end
-function TerminalElement:getHeight()
-	return self.screen.height
-end
-
--- Draw screen
 function TerminalElement:terminalPaint()
 	local bbox = self:inner(self.bbox)
-	-- Draw each strip in the screen
-	for lineY,line in ipairs(self.screen.strips) do
-		for i,strip in ipairs(line) do
-			if self.needsRepaint or strip.dirty then
-				local x = bbox.x + strip:left() - 1
-				local y = bbox.y + lineY - 1
-				self:draw(x, y, strip.str, strip.text, strip.back, bbox)
-			end
-		end
-	end
+	-- Draw terminal
+	self.term:draw(bbox, self.needsRepaint)
 	-- Update cursor blink
 	self:updateTerminalBlink()
 end
 function TerminalElement:terminalResize()
 	local bbox = self:inner(self.bbox)
-	self.screen:updateSize(bbox.w, bbox.h, self.back)
-end
-
--- Delegated methods to capture changes
-function TerminalElement:write(str)
-	--local len = math.min(#str, math.max(0, self:getWidth() - self.curX + 1))
-	local len = #str
-	local strip = self.screen:write(str, self.curX, self.curY, self.text, self.back)
-	self.curX = self.curX + len
-	self:markPaint()
-end
-function TerminalElement:getSize()
-	return self:getWidth(), self:getHeight()
-end
-function TerminalElement:getCursorPos()
-	return self.curX, self.curY
-end
-function TerminalElement:isColor()
-	return true
-end
-function TerminalElement:isColour()
-	return self:isColor()
-end
-function TerminalElement:setCursorPos(x, y)
-	self.curX, self.curY = x, y
-	self:markPaint()
-end
-function TerminalElement:setTextColor(text)
-	self.text = text
-	self:markPaint()
-end
-function TerminalElement:setTextColour(text)
-	return self:setTextColor(text)
-end
-function TerminalElement:setBackgroundColor(back)
-	self.back = back
-	self:markPaint()
-end
-function TerminalElement:setBackgroundColour(back)
-	return self:setBackgroundColor(back)
-end
-function TerminalElement:setCursorBlink(blink)
-	self.blink = blink or false
-	self:markPaint()
-end
-function TerminalElement:clearLine()
-	self.screen:clearLine(self.curY, self.back, false)
-	self:markPaint()
-end
-function TerminalElement:clear()
-	self.screen:clear(self.back, false)
-	self:markPaint()
-end
-function TerminalElement:scroll(n)
-	self.screen:scroll(n, self.back, false)
-	self:markPaint()
+	-- Update terminal size
+	self.term:updateSize(bbox)
 end
 
 -- Exports
---
+++ lib/ccgui/paint/ElementTerminal.lua
@@ -0,0 +1,126 @@
+--[[
+
+	ComputerCraft GUI
+	Terminal drawing to an element
+
+--]]
+
+local Object			= require "objectlua.Object"
+local BufferedScreen	= require "ccgui.paint.BufferedScreen"
+
+-- Terminal which draws on an element
+local ElementTerminal = Object:subclass("ccgui.paint.ElementTerminal")
+function ElementTerminal:initialize(element)
+	-- Target element
+	self.element = element
+	-- Screen
+	self.screen = BufferedScreen:new(0, 0)
+	-- Terminal state
+	self.curX, self.curY = 1, 1
+	self.text = colours.white
+	self.back = colours.black
+	self.blink = false
+end
+
+function ElementTerminal:export()
+	-- Export to term API
+	local exported = {}
+	for k,v in pairs(term) do
+		if self[k] == nil then
+			-- Delegate to terminal
+			exported[k] = function(self, ...)
+				f(...)
+			end
+		else
+			-- Redirect to own method
+			exported[k] = function(...)
+				return self[k](self, ...)
+			end
+		end
+	end
+	return exported
+end
+
+function ElementTerminal:draw(bbox, repaint)
+	-- Draw each strip in the screen
+	for lineY,line in ipairs(self.screen.strips) do
+		for i,strip in ipairs(line) do
+			if repaint or strip.dirty then
+				local x = bbox.x + strip:left() - 1
+				local y = bbox.y + lineY - 1
+				self.element:draw(x, y, strip.str, strip.text, strip.back, bbox)
+			end
+		end
+	end
+end
+function ElementTerminal:updateSize(bbox)
+	self.screen:updateSize(bbox.w, bbox.h, self.back, true)
+end
+function ElementTerminal:getBlinkState(bbox)
+	local x = bbox.x + self.curX - 1
+	local y = bbox.y + self.curY - 1
+	return self.blink, x, y, self.text
+end
+
+function ElementTerminal:write(str)
+	--local len = math.min(#str, math.max(0, self:getWidth() - self.curX + 1))
+	local len = #str
+	local strip = self.screen:write(str, self.curX, self.curY, self.text, self.back)
+	self.curX = self.curX + len
+	self.element:markPaint()
+end
+function ElementTerminal:getWidth()
+	return self.screen.width
+end
+function ElementTerminal:getHeight()
+	return self.screen.height
+end
+function ElementTerminal:getSize()
+	return self:getWidth(), self:getHeight()
+end
+function ElementTerminal:getCursorPos()
+	return self.curX, self.curY
+end
+function ElementTerminal:isColor()
+	return true
+end
+function ElementTerminal:isColour()
+	return self:isColor()
+end
+function ElementTerminal:setCursorPos(x, y)
+	self.curX, self.curY = x, y
+	self.element:markPaint()
+end
+function ElementTerminal:setTextColor(text)
+	self.text = text
+	self.element:markPaint()
+end
+function ElementTerminal:setTextColour(text)
+	return self:setTextColor(text)
+end
+function ElementTerminal:setBackgroundColor(back)
+	self.back = back
+	self.element:markPaint()
+end
+function ElementTerminal:setBackgroundColour(back)
+	return self:setBackgroundColor(back)
+end
+function ElementTerminal:setCursorBlink(blink)
+	self.blink = blink or false
+	self.element:markPaint()
+end
+function ElementTerminal:clearLine()
+	self.screen:clearLine(self.curY, self.back, true)
+	self.element:markPaint()
+end
+function ElementTerminal:clear()
+	self.screen:clear(self.back, true)
+	self.element:markPaint()
+end
+function ElementTerminal:scroll(n)
+	self.screen:scroll(n, self.back, true)
+	self.element:markPaint()
+end
+
+-- Exports
+return ElementTerminal
