02b981a1b46948df26956880a36bc1c56870b248
Mattias Buelens <mattias@buelens.com>
2014-02-03 21:19:48 +0100

    Changed TaskBar into a custom MenuBar.

--- lib/ccgui/os/TaskBar.lua
+++ lib/ccgui/os/TaskBar.lua
@@ -6,16 +6,14 @@
 --]]
 
 local FlowContainer	= require "ccgui.FlowContainer"
-local Button		= require "ccgui.Button"
+local MenuBar		= require "ccgui.MenuBar"
+local MenuBarButton	= require "ccgui.MenuBarButton"
 local Margins		= require "ccgui.geom.Margins"
 local StartMenu		= require "ccgui.os.StartMenu"
 local ClockDisplay	= require "ccgui.os.ClockDisplay"
 
-local TaskBar = FlowContainer:subclass("ccgui.os.TaskBar")
+local TaskBar = MenuBar:subclass("ccgui.os.TaskBar")
 function TaskBar:initialize(opts)
-	opts.horizontal = true
-	opts.background = opts.background or colours.lightGrey
-
 	super.initialize(self, opts)
 
 	-- Target window container
@@ -28,12 +26,18 @@ function TaskBar:initialize(opts)
 	self.clockForeground = opts.clockForeground or colours.grey
 	self.clockBackground = opts.clockBackground or self.barBackground
 	
-	self.startButton = Button:new{
+	self.startMenu = StartMenu:new{
+		windows = self.windows
+	}
+	self.startButton = MenuBarButton:new{
 		text = "Start",
+		menu = self.startMenu,
+		menuUp = true,
 		padding = Margins:new(0, 1),
 		foreground = self.startForeground,
 		background = self.startBackground
 	}
+	self:addMenu(self.startButton)
 	self.bar = FlowContainer:new{
 		stretch = true,
 		horizontal = true,
@@ -44,75 +48,10 @@ function TaskBar:initialize(opts)
 		foreground = self.clockForeground,
 		background = self.clockBackground
 	}
-	self:add(self.startButton, self.bar, self.clockText)
+	self:add(self.bar, self.clockText)
 	
-	self.menu = StartMenu:new{
-		windows = self.windows
-	}
-	self:add(self.menu)
-
-	self.startButton:on("buttonpress", self.toggleMenu, self)
-	self:on("window_background", self.closeMenu, self)
-	self:on("mouse_click", self.foregroundOnClick, self)
-end
-
-function TaskBar:isMenuOpen()
-	return self.menu:isMenuOpen()
-end
-function TaskBar:openMenu()
-	assert(self.bbox ~= nil, "taskbar missing layout")
-	self.menu:openMenu(self.bbox.x, self.bbox.y - 1, true)
-end
-function TaskBar:closeMenu()
-	self.menu:closeMenu()
-end
-function TaskBar:toggleMenu()
-	if not self:isMenuOpen() then
-		self:openMenu()
-	else
-		self:closeMenu()
-	end
-end
-
-function TaskBar:layout(bbox)
-	-- Clip to bottom of parent container
-	if self.parent then
-		local pbox = self.parent:inner(self.parent.bbox)
-		bbox.y = pbox.y + pbox.h - bbox.h
-	end
-	super.layout(self, bbox)
-end
-function TaskBar:contains(x, y)
-	return super.contains(self, x, y) or (self:isMenuOpen() and self.menu:contains(x, y))
-end
-
--- Window-like behaviour
-function TaskBar:isForeground()
-	if not self:isVisible() then
-		return false
-	end
-	if self.parent then
-		return self == self.parent:getForegroundWindow()
-	end
-	return true
-end
-function TaskBar:bringToForeground()
-	self.parent:bringToForeground(self)
-end
-function TaskBar:markRepaint()
-	if not self.needsRepaint then
-		-- Repaint parent window container
-		if self.parent ~= nil then
-			self.parent:markRepaint()
-		end
-	end
-	super.markRepaint(self)
-end
-function TaskBar:foregroundOnClick(button, x, y)
-	-- Bring to foreground on click
-	if button == 1 and self:isVisible() and self:contains(x, y) then
-		self:bringToForeground()
-	end
+	--self:on("window_background", self.closeMenu, self)
+	--self:on("mouse_click", self.foregroundOnClick, self)
 end
 
 -- Exports
